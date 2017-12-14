65c10b2d20da78df082cd0063d8ffafa4a73a736
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-12-15 00:40:05 +0100

    scripts

---
+++ linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+npm i -g @angular/cli
+if [-d empty_project]
+then
+    rm -R empty_project/
+fi
+mkdir empty_project
+cd empty_project
+ng new ng2
+mkdir server
+cd server
+npm init server
+npm i --save express bootstrap debug debug-sx
+npm i --save-dev @types/debug @types/express @types/node del gulp gulp-changed gulp-sourcemaps gulp-typescript gulp-using merge-stream run-sequence typescript
+code package.json
+cd ../..
+rsync -aP gulpfile.js empty_project/server/gulpfile.js
---
+++ windows.cmd
@@ -0,0 +1,19 @@
+@ECHO OFF
+SET interactive=0
+ECHO %CMDCMDLINE% | FINDSTR /L %COMSPEC% >NUL 2>&1
+IF %ERRORLEVEL% == 0 SET interactive=1
+update_angular.sh
+IF EXIST "empty_project" rmdir empty_project /s /q
+mkdir empty_project
+cd empty_project
+ng new ng2
+mkdir server
+cd server
+npm init server
+npm i --save express bootstrap debug debug-sx
+npm i --save-dev @types/debug @types/express @types/node del gulp gulp-changed gulp-sourcemaps gulp-typescript gulp-using merge-stream run-sequence typescript
+code package.json
+cd ..\..
+copy gulpfile.js empty_project\server\gulpfile.js
+IF "%interactive%"=="0" PAUSE
+EXIT /B 0
