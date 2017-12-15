254bb291621fc709a16cde2f8129957f92a8a124
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-12-15 01:13:11 +0100

    scripts

--- linux.sh
+++ linux.sh
@@ -13,5 +13,17 @@ npm init server
 npm i --save express bootstrap debug debug-sx
 npm i --save-dev @types/debug @types/express @types/node del gulp gulp-changed gulp-sourcemaps gulp-typescript gulp-using merge-stream run-sequence typescript
 code package.json
-cd ../..
+mkdir src
+cd src
+cat <<EOF >main.ts
+import * as path from 'path';
+import * as http from 'http';
+import * as child from 'child_process';
+import * as fs from 'fs';
+
+import * as express from 'express';
+import * as bodyparser from 'body-parser';
+import * as debugsx from 'debug-sx';
+EOF
+cd ../../..
 rsync -aP gulpfile.js empty_project/server/gulpfile.js
--- windows.cmd
+++ windows.cmd
@@ -13,7 +13,19 @@ npm init server
 npm i --save express bootstrap debug debug-sx
 npm i --save-dev @types/debug @types/express @types/node del gulp gulp-changed gulp-sourcemaps gulp-typescript gulp-using merge-stream run-sequence typescript
 code package.json
-cd ..\..
+mkdir src
+cd src
+(
+@echo import * as path from 'path';
+@echo import * as http from 'http';
+@echo import * as child from 'child_process';
+@echo import * as fs from 'fs';
+@echo 
+@echo import * as express from 'express';
+@echo import * as bodyparser from 'body-parser';
+@echo import * as debugsx from 'debug-sx';
+) > main.ts
+cd ..\..\..
 copy gulpfile.js empty_project\server\gulpfile.js
 IF "%interactive%"=="0" PAUSE
 EXIT /B 0
